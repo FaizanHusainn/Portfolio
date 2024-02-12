67bcb8aeabb4e637e182e4e918796aabdf238947
Faizan Husain <114300196+FaizanHusainn@users.noreply.github.com>
2024-02-13 01:06:06 +0530

    delete

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Footer.css'
-const Footer = () => {
-  return  <div className='footer'>" Crafting digital masterpieces, pixel by pixel "</div>
-  
-}
-
-export default Footer
